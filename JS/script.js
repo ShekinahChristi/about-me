8ce628c84761d063bb4c430cda97ce7ea0ad4cc6
new_username <new_email@example.com>
2023-06-09 04:34:06 +0000

    created a game using arrays and for loop and while loops

--- JS/script.js
+++ JS/script.js
@@ -1,64 +1,75 @@
 'use strict' 
 
-let username =prompt ('What is your name');
+let username = prompt ('What is your name');
 alert('Hello ' + username)
 console.log(`Say hey to ${username}`)
 
 function userinput(){
-  document.write(`Hi ${username} its been a while`)
+  document.write(`Hi ${username} I had fun playing with you.`)
 
 }
 userinput()
 
-let welcome =prompt ('Welcome to my world!!')
+let welcome = prompt ('Welcome to my world!!')
 console.log('welcome user to Site');
 
-let play =prompt('Ready to Play A Game');
+let play = prompt('Ready to Play A Game');
 switch(play.toLowerCase())
 {
   case 'yes':
-    console.log('Lets Play A Game')
-    alert('Lets Go Play!!')
-  case 'no':
-    console.log('ok Maybe Next Time')
-    alert('ok Maybe Next Time')
+    console.log('Lets Play A Game');
+    alert('Lets Go Play!!');
     break;
+  case 'no':
+    alert('Ok Maybe Next Time')
+    break;
+    default:
+    console.log('ok Maybe Next Time');
 }
 
-let job =prompt('My first job was at Gamestop');
+let job = prompt('My first job was at Gamestop');
 switch(job.toLowerCase())
 {
   case 'yes':
-    console.log('Correct')
-    alert('Yes Correct Best Job Ever!!')
-  case 'no':
-    console.log('Incorrect')
-    alert('Incorrect read the Bio again ')
+    console.log('Correct');
+    alert('Yes Correct Best Job Ever!!');
     break;
+  case 'no':
+    console.log('Incorrect');
+    alert('Incorrect read the Bio again');
+    break;
+    default:
+ alert('Invalid');
 }
-
 let age = prompt('I am 35 years old ');
 switch(age.toLowerCase())
 {
   case 'yes':
-    console.log('False')
-    alert('No Im not that old!!')
-  case 'no':
-    console.log('Exactly thanks for paying attention')
-    alert('Exactly thanks for paying attention')
+    console.log('False');
+    alert('No Im not that old!!');
     break;
+  case 'no':
+    console.log('Exactly thanks for paying attention');
+    alert('Exactly thanks for paying attention');
+    break;
+    default:
+    alert('Invalid');
+    
 }
 
 let Tenn = prompt('I am from Tennesse');
 switch(Tenn.toLowerCase())
 { 
   case 'yes':
-  console.log('Wrong again')
-  alert('Wrong Again I am from Mississippi')
-case 'no':
-  console.log('Where are you from')  
-  alert('I see you read the Bio on my page')
+  console.log('Wrong again');
+  alert('Wrong Again I am from Mississippi');
   break;
+case 'no':
+  console.log('Where are you from');
+  alert('I see you read the Bio on my page');
+  break;
+  default:
+    alert('invalid');
 }
 
 let family = prompt('Do I have two siblings?');
@@ -66,13 +77,65 @@ switch(family.toLowerCase())
 {
   case 'yes':
   console.log(`You are Wrong!!`);
-  alert('You are Wrong!!')
-  break;
+  alert('You are Wrong!!');
+  break; 
+  
   case 'no':
-    console.log('Correct I have four beautiful siblings')
-    alert('Correct I have four beautiful siblings')
+    console.log('Correct I have four beautiful siblings');
+    alert('Correct I have four beautiful siblings');
     default:
-      console.log(`${username} thanks for stopping by to get to know me.`)
+      console.log(`${username} thanks for stopping by to get to know me.`);
   }
+
+  let guessday = "";
+  let i = 0;
+  while(guessday !== '3'){
+  console.log('guess the day')
+  guessday = prompt('Guess what day of june was i born');
+  if(guessday < 3){
+    alert('too low');
+  }else if (guessday > 3){
+      alert('too high');
+  }else if (guessday ==='3'){
+      alert('correct');
+   break;
+  
+   }
+ i=i+1;
+ if(i===5){
+  alert('too many attempts')
+  break;
+ }
+ }
+
+ let userpoints = 0;
+ let listofitems = ['strawberries','blueberries','peaches','watermelon'];
+
+ for (let i = 0; i < listofitems.length; i=i+1) {
+  console.log(listofitems[i]);
+listofitems = prompt('Guess the fruits I like');
+if(listofitems === 'strawberries'){
+alert('correct');
+}else if(listofitems === 'peaches'){
+alert('correct');
+}else if(listofitems === 'blueberries')
+alert('correct');
+else if(listofitems ==='watermelon'){
+alert('correct');
+if (listofitems ==='')
+alert('Try again')
+break;
+  
+
+}
+
+if (i === 6) {
+  userPoints++;
+} alert('better luck next time');
+alert(`you have ${userPoints} points`);
+ break;
+}
+  
+
   
   
